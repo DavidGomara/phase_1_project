8a7591baf28e663325d94d39b3bab373b936947a
David Gomara <71353589+DavidGomara@users.noreply.github.com>
2020-12-06 22:02:36 -0500

    Create environment.rb

---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+  
+require "pry"
+require "unirest"
+
+require_relative "./services/api"
+require_relative "./services/cli"
+require_relative "./models/categories"
+require_relative "./models/jokes"
