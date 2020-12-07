b0b5cc8c3ef6a11494503b7c73aa58b9bce762ec
David Gomara <71353589+DavidGomara@users.noreply.github.com>
2020-12-06 21:51:11 -0500

    Update environment.rb

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 require "pry"
+require "unirest"
 
-require_relative "./models/characters"
 require_relative "./services/api"
 require_relative "./services/cli"
+require_relative "./models/categories"
+require_relative "./models/jokes"
